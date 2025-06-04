eb278ccad37312779b401a53707e6eaadeb74ce8
Bear210404 <araykerja@gmail.com>
2025-06-04 20:53:57 +0700

    Update page.jsx

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -2,7 +2,7 @@ export default function ClientHome() {
   return (
     <div>
       <h1 className="text-2xl font-bold">Welcome Client1</h1>
-      <p className="mt-2">Silakan reservasi ruangan dan lihat histori Anda.</p>
+      <p className="mt-2">Helo Silakan reservasi ruangan dan lihat histori Anda.</p>
     </div>
   );
 }
