9a916cbcdec9d7b851ceb2f1b27e944285284aef
Aryasatyawidyatna <aryasatyawidyatnaa@gmail.com>
2025-06-11 22:45:54 +0700

    oke

--- app/admin/history/page.jsx
+++ app/admin/history/page.jsx
@@ -1,6 +1,6 @@
 'use client';
-import React from 'react';
 
+import React from 'react';
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 import ProtectedPageWrapper from '@/components/ProtectedPageWrapper';
