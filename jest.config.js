3804eeb40fbd0cd983c7c6b4667e77692f323a5d
Aryasatyawidyatna <aryasatyawidyatnaa@gmail.com>
2025-06-11 21:16:48 +0700

    boom

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,19 @@
-module.exports = {
-testEnvironment: 'jest-environment-jsdom',
-setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './', // path ke root Next.js project
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    // Mengabaikan import CSS, SCSS, dan sejenis
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
 };
+
+module.exports = createJestConfig(customJestConfig);
