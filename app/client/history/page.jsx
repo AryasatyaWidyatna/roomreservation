0550048cf4e6bbc43f47b973d770dc1fb26e1c52
Bear210404 <araykerja@gmail.com>
2025-06-16 23:50:47 +0700

    Update page.jsx

--- app/client/history/page.jsx
+++ app/client/history/page.jsx
@@ -99,6 +99,7 @@ function ClientHistory() {
                 className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors"
               > <a href="../login">
                 Logout
+                </a>
               </button>
             </div>
 
