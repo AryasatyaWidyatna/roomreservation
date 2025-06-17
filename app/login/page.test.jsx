bf5c1aeeea84395bbbc8c8f4a946ca2ddde014c5
Bear210404 <araykerja@gmail.com>
2025-06-17 10:48:41 +0700

    Update page.test.jsx

--- app/login/page.test.jsx
+++ app/login/page.test.jsx
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom';
 import React from 'react';
 
 const SUPABASE_URL = 'https://cradzbdfhvrzcmlpumrn.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh...';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyYWR6YmRmaHZyemNtbHB1bXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1OTA1MTksImV4cCI6MjA2NDE2NjUxOX0.qCJgJ1kuk0RTTw7ACL3AWC9AccOEer6hj8FegmYac4k';
 
 describe('LoginPage', () => {
   test('renders login heading and form elements', () => {
